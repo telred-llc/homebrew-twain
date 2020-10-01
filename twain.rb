9c8647a16c288cae6f8ee3b2d89052fee47e07f2
admin <tranhailong1998@gmail.com>
2020-10-01 17:35:51 +0700

    a

--- twain.rb
+++ twain.rb
@@ -5,7 +5,7 @@ class Twain < Formula
   desc "Cross-platform Text Expander written in Rust"
   homepage "https://github.com/telred-llc/twain"
   url "https://github.com/telred-llc/twain/files/5310936/twain.tar.gz"
-  sha256 "53ae68588564dc5d65ecafdac0f3b5114feeebc6a903aec357d72778e909a218"
+  sha256 "1cc428ea725a216f249b9d11723fe34038449b32d91adc2314d0dab42b09b3fc"
   version "1.0"
   
   #depends_on "cmake" => :build 
@@ -14,7 +14,7 @@ class Twain < Formula
   
   resource "twain" do 
   	url "https://github.com/telred-llc/twain/files/5310935/twain_service.tar.gz"
-  	sha256 "4589cbf96b09dcabeb9d062b1025c485ae6883f9dc71f5582c5874063d5d8065"
+  	sha256 "13254e17bf70965290b5a760d7d045ce2b0fcf6d49fd6c58e3bf2c2cc60c28e7"
   end
   
   def install
