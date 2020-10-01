075b726cb25f1cb44e8b00433ff7cc045a424c07
admin <tranhailong1998@gmail.com>
2020-10-01 16:02:39 +0700

    add twain_service

--- twain.rb
+++ twain.rb
@@ -8,9 +8,9 @@ class Twain < Formula
   sha256 "972e8544baee1fc2cdfbeb2d94e54228d89506153e9f494ffa5b38b5ff7b15da"
   version "1.0"
   
-  depends_on "cmake" 
-  depends_on "cpprestsdk" 
-  depends_on "rabbitmq-c"
+  #depends_on "cmake" => :build 
+  depends_on "cpprestsdk" =>:build  
+  depends_on "rabbitmq-c" =>:build
   
   resource "twain" do 
   	url "https://github.com/telred-llc/twain/files/5310461/twain_service.tar.gz"
